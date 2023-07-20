4a4a31a157e3279c50507c1fe0f23dbcbeb691b0
S2kStranger <s2kkasera@gmail.com>
2023-07-20 16:24:04 +0530

    deployed CSS HTML files

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 const bodyParser=require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname,'public')));
 
 const loginPage=require('./login.js');
 app.use(loginPage);
 
 const chatPage=require('./chat.js');
 app.use(chatPage);
+
+app.get('/contactus',(req,res,next) => {
+    res.sendFile(path.join(__dirname,'views','contactus.html'));
+})
+
+app.post('/success',(req,res,next) => {
+    res.sendFile(path.join(__dirname,'views','success.html'));
+})
+
+
+// app.use((req,res,next) => {
+//     res.status(404).sendFile(path.join(__dirname,'views','404Error.html'));
+// })
 
 app.listen(3000);
--- login.js
+++ login.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const rootDir=require('./utilhelper/path.js');
 
 router.get('/login', (req, res, next) => {
     //redirecting to '/' file 
-    res.send('<form action="/" method="POST" onsubmit=localStorage.setItem("username",document.getElementById("username").value);><input type="text" name="username" id="username"><button type="submit">Send</button></form>')
+    //res.send('<form action="/" method="POST" onsubmit=localStorage.setItem("username",document.getElementById("username").value);><input type="text" name="username" id="username"><button type="submit">Send</button></form>')
+    res.sendFile(path.join(rootDir,'views','login.html'));
 });
 
 module.exports = router;
